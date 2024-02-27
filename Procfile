b44555bdc7a5e5869a10e29f8908dfe8e6246949
bogdan9876 <bogdankryveiko@gmail.com>
2024-02-27 21:37:24 +0200

    add next step for succesful deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web :gunicorn wsgi:app
+web: waitress-serve --call app.main:create_app
--- app/main.py
+++ app/main.py
@@ -1,33 +1,38 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 from app.torch_utils import transform_image, get_prediction
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
+    def allowed_file(filename):
+        return "." in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def allowed_file(filename):
-    return "." in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    @app.route('/predict', methods=['POST'])
+    @cross_origin()
+    def predict():
+        if request.method == 'POST':
+            file = request.files.get('file')
+            if file is None or file.filename == "":
+                return jsonify({'error': 'no file'})
+            if not allowed_file(file.filename):
+                return jsonify({'error': 'format not supported'})
 
+            img_bytes = file.read()
+            tensor = transform_image(img_bytes)
+            prediction = get_prediction(tensor)
+            data = {'prediction': prediction.item(), 'class_name': str(prediction.item())}
+            return jsonify(data)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        file = request.files.get('file')
-        if file is None or file.filename == "":
-            return jsonify({'error': 'no file'})
-        if not allowed_file(file.filename):
-            return jsonify({'error': 'format not supported'})
+        return jsonify({'result': 1})
 
-        img_bytes = file.read()
-        tensor = transform_image(img_bytes)
-        prediction = get_prediction(tensor)
-        data = {'prediction': prediction.item(), 'class_name': str(prediction.item())}
-        return jsonify(data)
+    return app
 
-    return jsonify({'result': 1})
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
